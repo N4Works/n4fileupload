ca44fa7fa10f7350376fa0fc9f9a2d0ada4e1d76
Tiago Resende <tiagor87@gmail.com>
2015-08-12 17:57:41 -0300

    Compila os arquivos e ajusta exemplo

--- dist/n4fileupload.js
+++ dist/n4fileupload.js
@@ -16,37 +16,80 @@
     .directive('n4FileUploadDirective', [
       '$timeout',
       'n4FileUploadService',
-      function ($timeout, service) {
+      '$q',
+      '$log',
+      function ($timeout, service, $q, $log) {
         return {
           require: 'ngModel',
-          restrict: 'A',
+          restrict: 'AE',
+          replace: true,
+          transclude: true,
           scope: {
-            text: '@',
-            notify: '&n4FileUploadDirective'
+            onStart: '&',
+            onFinish: '&'
           },
           template: [
-            '<label>',
-            '  <figure></figure>',
-            '  {{text}}',
-            '  <input class="bt-arquivo" type="file"/>',
-            '</label>'
+            '<div>',
+            '  <label class="bt">',
+            '    <span ng-transclude=""></span>',
+            '    <input class="bt-input" type="file"/>',
+            '  </label>',
+            '  <ul class="files">',
+            '    <li ng-repeat="file in files">',
+            '      <span ng-bind="file.name"></span>',
+            '      <span ng-bind="file.progress"></span>',
+            '    </li>',
+            '  <ul>',
+            '</div>'
           ].join(''),
-          replace: true,
           link: function (scope, element, attrs, controller) {
-            element.find('label').addClass(attrs.buttonClass);
-            element.find('figure').addClass(attrs.iconClass);
-            element.find('input').addClass(attrs.inputClass);
+            var input = element.find('input');
 
-            element.on('change', function (event) {
-              service.send(event.target.files)
-                .then(function (data) {
+            if (!!attrs.multiple) {
+              input.attr('multiple', 'multiple');
+            }
+
+            scope.files = [];
+            input.on('change', function (event) {
+              scope.onStart();
+
+              element.addClass('sending');
+              input.prop('disabled', true);
+
+              var promises = [],
+                files = event.target.files,
+                promise;
+              for (var i = (files.length - 1); i >= 0; i -= 1) {
+                scope.files.push(files[i]);
+
+                promise = service.send(files[i])
+                  .then(null,
+                  function (e) {
+                    $log.error(e);
+                  }, function (event) {
+                    var file = event.config.file;
+                    file.progress = parseInt(event.loaded * 100 / event.total, 10) + '%';
+                  });
+
+                promises.push(promise);
+              }
+
+              $q.all(promises)
+                .then(function (responses) {
+                  var data = responses.map(function (x) {
+                    return x.data[0];
+                  });
                   controller.$setViewValue(data);
-                  scope.notify();
+              })
+                .finally(function () {
+                  element.removeClass('sending');
+                  input.prop('disabled', false);
+                  scope.onFinish();
                 });
             });
 
             scope.$on('$destroy', function () {
-              element.off('change');
+              input.off('change');
             });
           }
         };
@@ -58,35 +101,20 @@
 ;
 (function (ng) {
   ng
-    .module('n4FileUpload.services', [])
+    .module('n4FileUpload.services', ['ngFileUpload'])
     .provider('n4FileUploadService', function () {
       var self = this;
       this.endpoint = '';
 
       this.$get = [
-        '$http',
-        function ($http) {
+        'Upload',
+        function (Upload) {
           return {
-            send: function (files, endpoint) {
-              var formData = new FormData();
-              for (var i = (files.length - 1); i >= 0; i -= 1) {
-                formData.append('file_' + i, files[i]);
-              }
-
-              return $http({
-                method: 'POST',
+            send: function (file, endpoint) {
+              return Upload.upload({
                 url: endpoint || self.endpoint,
-                headers: {'Content-Type': undefined},
-                data: formData
-              }).then(function (response) {
-                return response.data;
+                file: file
               });
-            },
-            delete: function (fileUrl, endpoint) {
-              return $http.delete(endpoint || self.endpoint, {params: {url: fileUrl}})
-                .then(function (response) {
-                  return response.data;
-                });
             }
           };
         }];
